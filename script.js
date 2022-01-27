1608bbad4583e2c490d02063c3353af2f2a03d4c
Nick <Nick_II2000@mail.ru>
2022-01-27 16:45:39 +0600

    Fourth commit

--- script.js
+++ script.js
@@ -115,3 +115,20 @@ const c1 = increment();
 const c2 = increment();
 const c3 = increment();
 console.log('example increment', c1, c2, c3);
+
+
+
+let c = 4;
+
+function addX(x) {
+    return function (n) {
+        return n + ++x;
+    };
+}
+const addThree = addX(3);
+let d = addThree(c);
+console.log('example partial application', d);
+d = addThree(c);
+console.log('example partial application', d);
+d = addThree(c);
+console.log('example partial application', d);
