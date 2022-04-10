7721cc96632431725a5bd1f49731dc44b457c9c7
Nick <Nick_II2000@mail.ru>
2022-04-10 21:04:36 +0600

    16-th commit

--- script.js
+++ script.js
@@ -170,7 +170,7 @@ console.log(counter()); // 12
 console.log(counter()); // 13
 
 // создать другой счётчик, он будет независим от первого
-var counter2 = makeCounter(999);
+var counter2 = makeCounter(888);
 console.log(counter2()); // 1
 
 // var f = ([a, b] = [1, 2], {x: c} = {x: a + b}) => a + b + c;
